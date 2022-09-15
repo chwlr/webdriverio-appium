bc45505e72a457b7209deeea137d3f2357cc4fe0
Chris Rondonuwu <vaelvex@Chriss-MacBook-Pro.local>
2022-09-16 02:25:21 +0800

    update config & add test sample

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 exports.config = {
     //
     // ====================
@@ -51,18 +53,11 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-    
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
+        platformName: "Android",
+        "appium:platformVersion": "11",
+        "appium:deviceName": "Pixel 5 API 30",
+        "appium:automationName": "UIAutomator2",
+        "appium:app": path.join(process.cwd(), "./app/android/ApiDemos-debug.apk")
     }],
     //
     // ===================
@@ -111,7 +106,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver','appium'],
+    services: ['appium'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
